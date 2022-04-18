69bf1013a0bdd3cc8d7dfcd03b5736bae6a5aa37
abhishekgoyal23 <abhishek.goyal@abcfitness.com>
2022-04-18 18:00:16 +0530

    adding some more test scenario for payment method

--- src/test/java/com/bh/assign/runner/RunnerTest.java
+++ src/test/java/com/bh/assign/runner/RunnerTest.java
@@ -5,7 +5,9 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         plugin = {"pretty", "html:target/cucumber-report.html","json:target/cucumber/Destination/cucumber.json"},
         monochrome = true,
-//        tags="@BillingDemo",
+//        tags="@BillingDemo",@DemoBilling_post_PaymentMethodData
+            tags="@DemoBilling_post_PaymentMethodData",
+
         features = {"src/test/resources"},glue={"com.bh.assign.stepdef"}
 )
 
